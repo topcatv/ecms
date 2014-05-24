1be6ac4ff859f386e22679faeeec7f8d51118895
ccuper <20739609@qq.com>
2014-05-24 10:44:14 +0800

    角色

---
+++ src/main/webapp/app/view/TabPanel.js
@@ -0,0 +1,50 @@
+Ext.define('ECM.view.TabPanel', {
+	extend : 'Ext.tab.Panel',
+	title : 'User Info',
+	alias : 'widget.edituser',
+	defaults : {
+		bodyStyle : 'padding:15px'
+	},
+	items : [ {
+		title : 'General Info',
+		xtype : 'form',
+		items : [ {
+			xtype : 'textfield',
+			fieldLabel : 'First Name',
+			value : 'Vitaliy',
+			allowBlank : false
+		}, {
+			xtype : 'textfield',
+			fieldLabel : 'Last Name',
+			value : 'Khmurach',
+			allowBlank : false
+		} ],
+		buttons : [ {
+			text : 'Save',
+			action : 'save',
+			disabled : true
+		} ]
+	}, {
+		title : 'Additional Info',
+		xtype : 'form',
+		items : [ {
+			xtype : 'textfield',
+			fieldLabel : 'Country',
+			value : 'Ukraine',
+			allowBlank : false
+		}, {
+			xtype : 'textfield',
+			fieldLabel : 'City',
+			value : 'Kiev',
+			allowBlank : false
+		} ],
+		buttons : [ {
+			text : 'Save',
+			action : 'save',
+			disabled : true
+		} ]
+	} ],
+	initComponent : function() {
+		this.callParent(arguments);
+	},
+});
